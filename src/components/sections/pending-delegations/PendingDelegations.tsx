5b320cde33fa70f32e06b7b514518e70e09c191a
Guillermo Diéguez <guillermodieguez@outlook.com>
2022-01-27 02:30:55 -0300

    add table pagination

--- src/components/sections/pending-delegations/PendingDelegations.tsx
+++ src/components/sections/pending-delegations/PendingDelegations.tsx
@@ -12,8 +12,10 @@ import {
   Tooltip,
   Typography,
   Button,
+  styled,
+  TablePagination,
 } from '@mui/material';
-import React, { useContext, useEffect } from 'react';
+import React, { ChangeEvent, useContext, useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import CheckIcon from '@mui/icons-material/Check';
 import RefreshIcon from '@mui/icons-material/Refresh';
@@ -24,13 +26,38 @@ import EmptyData from '../../common/empty-data/EmptyData';
 import { walletProviderContext } from '../../providers/WalletProvider';
 import Forbidden from '../../common/forbidden/Forbidden';
 
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
 const PendingDelegations: React.FC = () => {
   const { getPendingDelegations, acceptProduct } = useProductFactory();
   const { contract, pendingDelegations } = useContext(productFactoryContext);
   const { connected } = useContext(walletProviderContext);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const emptyRows =
+    page > 0
+      ? Math.max(0, (1 + page) * rowsPerPage - pendingDelegations.length)
+      : 0;
 
   const handleAcceptProduct = async (productId: number) => {
     await acceptProduct(productId);
+  };
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
   };
 
   useEffect(() => {
@@ -71,34 +98,54 @@ const PendingDelegations: React.FC = () => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {pendingDelegations.map((product) =>
-                product ? (
-                  <TableRow
-                    key={uuidv4()}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell align="center">{product.id}</TableCell>
-                    <TableCell component="th" scope="row">
-                      {product.name}
-                    </TableCell>
-                    <TableCell align="center">{product.status}</TableCell>
-                    <TableCell align="right">{product.owner}</TableCell>
-                    <TableCell align="right">{product.newOwner}</TableCell>
-                    <TableCell align="center" padding="checkbox">
-                      <Tooltip title="Accept">
-                        <IconButton
-                          aria-label="accept delegation"
-                          onClick={() => handleAcceptProduct(product.id)}
-                        >
-                          <CheckIcon />
-                        </IconButton>
-                      </Tooltip>
-                    </TableCell>
-                  </TableRow>
-                ) : null,
+              {pendingDelegations
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((product) =>
+                  product ? (
+                    <StyledTableRow
+                      key={uuidv4()}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell align="center">{product.id}</TableCell>
+                      <TableCell component="th" scope="row">
+                        {product.name}
+                      </TableCell>
+                      <TableCell align="center">{product.status}</TableCell>
+                      <TableCell align="right">{product.owner}</TableCell>
+                      <TableCell align="right">{product.newOwner}</TableCell>
+                      <TableCell align="center" padding="checkbox">
+                        <Tooltip title="Accept">
+                          <IconButton
+                            aria-label="accept delegation"
+                            onClick={() => handleAcceptProduct(product.id)}
+                          >
+                            <CheckIcon />
+                          </IconButton>
+                        </Tooltip>
+                      </TableCell>
+                    </StyledTableRow>
+                  ) : null,
+                )}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: 53 * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
               )}
             </TableBody>
           </Table>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 20]}
+            component="div"
+            count={pendingDelegations.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
         </TableContainer>
       ) : (
         <EmptyData />
--- src/components/sections/product-display/ProductDisplay.tsx
+++ src/components/sections/product-display/ProductDisplay.tsx
@@ -12,8 +12,16 @@ import {
   Tooltip,
   Typography,
   Button,
+  styled,
+  TablePagination,
 } from '@mui/material';
-import React, { useCallback, useContext, useEffect, useState } from 'react';
+import React, {
+  ChangeEvent,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import IosShareIcon from '@mui/icons-material/IosShare';
 import RefreshIcon from '@mui/icons-material/Refresh';
@@ -23,11 +31,34 @@ import useProductFactory from '../../../hooks/product-factory.hook';
 import { productFactoryContext } from '../../providers/ProductFactoryProvider';
 import EmptyData from '../../common/empty-data/EmptyData';
 
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
 const ProductDisplay: React.FC = () => {
   const { getAllProducts } = useProductFactory();
   const [dialogOpen, setDialogOpen] = useState(false);
   const [selectedProductId, setSelectedProductId] = useState(0);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
   const { products, contract } = useContext(productFactoryContext);
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - products.length) : 0;
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   const handleDialogOpen = (id: number) => {
     setSelectedProductId(id);
@@ -82,34 +113,54 @@ const ProductDisplay: React.FC = () => {
               </TableRow>
             </TableHead>
             <TableBody>
-              {products.map((product) =>
-                product ? (
-                  <TableRow
-                    key={uuidv4()}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell align="center">{product.id}</TableCell>
-                    <TableCell component="th" scope="row">
-                      {product.name}
-                    </TableCell>
-                    <TableCell align="center">{product.status}</TableCell>
-                    <TableCell align="right">{product.owner}</TableCell>
-                    <TableCell align="right">{product.newOwner}</TableCell>
-                    <TableCell align="center" padding="checkbox">
-                      <Tooltip title="Delegate">
-                        <IconButton
-                          aria-label="delegate"
-                          onClick={() => handleDialogOpen(product.id)}
-                        >
-                          <IosShareIcon />
-                        </IconButton>
-                      </Tooltip>
-                    </TableCell>
-                  </TableRow>
-                ) : null,
+              {products
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((product) =>
+                  product ? (
+                    <StyledTableRow
+                      key={uuidv4()}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell align="center">{product.id}</TableCell>
+                      <TableCell component="th" scope="row">
+                        {product.name}
+                      </TableCell>
+                      <TableCell align="center">{product.status}</TableCell>
+                      <TableCell align="right">{product.owner}</TableCell>
+                      <TableCell align="right">{product.newOwner}</TableCell>
+                      <TableCell align="center" padding="checkbox">
+                        <Tooltip title="Delegate">
+                          <IconButton
+                            aria-label="delegate"
+                            onClick={() => handleDialogOpen(product.id)}
+                          >
+                            <IosShareIcon />
+                          </IconButton>
+                        </Tooltip>
+                      </TableCell>
+                    </StyledTableRow>
+                  ) : null,
+                )}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: 53 * emptyRows,
+                  }}
+                >
+                  <TableCell colSpan={6} />
+                </TableRow>
               )}
             </TableBody>
           </Table>
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 20]}
+            component="div"
+            count={products.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
         </TableContainer>
       ) : (
         <EmptyData />
